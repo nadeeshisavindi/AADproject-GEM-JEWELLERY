c2394e96c599efdec3db46ea1532cd985ee6351b
nadeeshisavindi <nadeeshisavindi@gmail.com>
2026-09-10 18:15:54 +0530

    update controllers

--- src/main/java/com/example/gemjewellery/controller/OrderController.java
+++ src/main/java/com/example/gemjewellery/controller/OrderController.java
@@ -3,14 +3,8 @@ package com.example.gemjewellery.controller;
 import com.example.gemjewellery.dto.CommonResponse;
 import com.example.gemjewellery.dto.OrderRequestDTO;
 import com.example.gemjewellery.entity.Order;
-import com.example.gemjewellery.repository.CustomerRepository;
-import com.example.gemjewellery.repository.JewelleryItemRepository;
-import com.example.gemjewellery.repository.OrderItemRepository;
 import com.example.gemjewellery.service.OrderService;
-import com.example.gemjewellery.service.impl.OrderServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -21,22 +15,20 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class OrderController {
 
-    private static final Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
-
-
     private final OrderService orderService;
-    private final OrderItemRepository orderItemRepository;
-    private final CustomerRepository customerRepository;
-    private final JewelleryItemRepository jewelleryItemRepository;
-
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public CommonResponse placeOrder(@RequestBody OrderRequestDTO request, Authentication authentication) {
+        Order order = orderService.placeOrder(authentication.getName(), request);
+        return new CommonResponse(0, order, "Order placed successfully");
     }
 
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public CommonResponse getAll()
+    public CommonResponse getAll() {
+
+        return new CommonResponse(0, orderService.getAllOrders(), "All orders");
+    }
 
 
     @GetMapping(value = "/customer/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
