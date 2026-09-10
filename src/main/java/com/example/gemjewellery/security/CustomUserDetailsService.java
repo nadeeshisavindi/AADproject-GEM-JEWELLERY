338c6dcc4f2f37bc849d4d20c84594d2759204e0
nadeeshisavindi <nadeeshisavindi@gmail.com>
2026-09-10 16:19:55 +0530

    updated

--- src/main/java/com/example/gemjewellery/security/CustomUserDetailsService.java
+++ src/main/java/com/example/gemjewellery/security/CustomUserDetailsService.java
@@ -17,6 +17,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with username: " + username));
 
     }
 }
