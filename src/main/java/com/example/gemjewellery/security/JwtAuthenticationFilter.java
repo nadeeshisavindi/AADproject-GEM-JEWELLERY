6ad49fca71419960d271deb7f0c43fdc16c4d1a2
nadeeshisavindi <nadeeshisavindi@gmail.com>
2026-09-10 16:26:05 +0530

    updated

--- src/main/java/com/example/gemjewellery/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/gemjewellery/security/JwtAuthenticationFilter.java
@@ -70,5 +70,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private void handleJwtException(HttpServletResponse response, int code, String message) throws IOException {
         response.setStatus(HttpStatus.OK.value());
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE)
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString
 }
