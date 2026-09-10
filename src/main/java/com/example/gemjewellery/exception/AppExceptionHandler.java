2d13507fef5c0971ef4eb750eb8aef8e4987309e
nadeeshisavindi <nadeeshisavindi@gmail.com>
2026-09-10 15:28:21 +0530

    exception project

--- src/main/java/com/example/gemjewellery/exception/AppExceptionHandler.java
+++ src/main/java/com/example/gemjewellery/exception/AppExceptionHandler.java
@@ -1,8 +1,9 @@
-
-        package com.example.gemjewellery.exception;
+package com.example.gemjewellery.exception;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 @ControllerAdvice
-public class AppExceptionHandler {
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
 }
