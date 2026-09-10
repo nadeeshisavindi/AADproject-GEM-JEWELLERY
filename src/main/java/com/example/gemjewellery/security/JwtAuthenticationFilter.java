2d89fa836e6642f48354c3a619e3550cbccfa6d5
nadeeshisavindi <nadeeshisavindi@gmail.com>
2026-09-10 16:21:48 +0530

    updated

--- src/main/java/com/example/gemjewellery/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/gemjewellery/security/JwtAuthenticationFilter.java
@@ -34,5 +34,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        String authHeader = request.getHeader("Authorization");
 
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            String token = authHeader.substring(7);
+            String username = jwtUtil.extractUsername(token);
+
+        }
 }
