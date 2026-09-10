8b46f8aeffe681bbd876c9417df870106021d2d6
nadeeshisavindi <nadeeshisavindi@gmail.com>
2026-09-10 16:23:00 +0530

    updated

--- src/main/java/com/example/gemjewellery/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/gemjewellery/security/JwtAuthenticationFilter.java
@@ -55,3 +55,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
             }
+            filterChain.doFilter(request, response);
