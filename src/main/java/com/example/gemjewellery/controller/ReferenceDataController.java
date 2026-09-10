abb30b2bc9778cb23458b5700d0f16db4b0bb579
nadeeshisavindi <nadeeshisavindi@gmail.com>
2026-09-10 15:15:14 +0530

    updated controllers

---
+++ src/main/java/com/example/gemjewellery/controller/ReferenceDataController.java
@@ -0,0 +1,27 @@
+package com.example.gemjewellery.controller;
+
+import com.example.gemjewellery.dto.CommonResponse;
+import com.example.gemjewellery.entity.GemColor;
+import com.example.gemjewellery.entity.GemOrigin;
+import com.example.gemjewellery.entity.GemType;
+import com.example.gemjewellery.entity.JewelleryCategory;
+import com.example.gemjewellery.entity.Supplier;
+import com.example.gemjewellery.repository.GemColorRepository;
+import com.example.gemjewellery.repository.GemOriginRepository;
+import com.example.gemjewellery.repository.GemTypeRepository;
+import com.example.gemjewellery.repository.JewelleryCategoryRepository;
+import com.example.gemjewellery.repository.SupplierRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+public class ReferenceDataController {
+
+    private final GemTypeRepository gemTypeRepository;
+
+
+}
